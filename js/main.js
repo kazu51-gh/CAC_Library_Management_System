b0f20c9193f64506eadbb9ba2dbb19173dda1ddb
kazu51-gh <kazu51git@gmail.com>
2023-04-30 11:42:18 +0900

    minor update

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ function search_func() {
     filter = input.value.toUpperCase();
     book_contents = Array.from(document.getElementsByClassName('a_book'));
     for (var i = 0; i < book_contents.length; i++) {
-        var text = book_contents[i].getElementsByClassName('book_title')[0].textContent;
+        // var text_only_name = book_contents[i].getElementsByClassName('book_title')[0].textContent; // only book title
         var text = book_contents[i].textContent;
-        if (text.toUpperCase().indexOf(filter) > -1) {
+        if (text.toUpperCase().indexOf(filter) !== -1) {
             book_contents[i].style.display = "";
         } else {
             book_contents[i].style.display = "none";
