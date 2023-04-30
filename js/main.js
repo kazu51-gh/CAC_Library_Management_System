2138923e7261a0e4ec9727e75ca4e1985c6e9cad
kazu51-gh <kazu51git@gmail.com>
2023-04-30 11:04:15 +0900

    add search function

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function search_func() {
+    var input = document.getElementById('input_keyword');
+    filter = input.value.toUpperCase();
+    book_contents = Array.from(document.getElementsByClassName('a_book'));
+    for (var i = 0; i < book_contents.length; i++) {
+        var text = book_contents[i].getElementsByClassName('book_title')[0].textContent;
+        var text = book_contents[i].textContent;
+        if (text.toUpperCase().indexOf(filter) > -1) {
+            book_contents[i].style.display = "";
+        } else {
+            book_contents[i].style.display = "none";
+        }
+    }
+}
